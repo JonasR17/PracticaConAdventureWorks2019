39717ea2c827dbbee8db3a598b6e519478ba8c42
Naels08 <romero99naels@gmail.com>
2024-06-04 14:28:48 -0600

    Actualizacion de consultas

--- Querys1.sql
+++ Querys1.sql
@@ -5,8 +5,8 @@
 --Obtén el número total de clientes en la tabla Customer.//
 --Muestra los nombres de todas las categorías de productos.//
 --Encuentra los empleados que tienen un correo electrónico que termina en '@adventure-works.com'.//
---Selecciona los nombres de los vendedores y sus territorios.
---Lista todos los clientes en la ciudad de 'London'.
+--Selecciona los nombres de los vendedores y sus territorios. 
+--Lista todos los clientes en la ciudad de 'London'. 
 --Cuenta cuántos productos tienen un precio de lista mayor a $1000.
 --Selecciona los 10 primeros empleados ordenados por su fecha de nacimiento.
 --Obtén el nombre y el precio de los productos que están descontinuados (discontinued).
@@ -17,7 +17,7 @@
 --Lista los nombres de los productos y las cantidades en stock de cada uno.
 --Encuentra todos los productos cuyo precio de lista sea mayor a $500 y que estén en la categoría 'Components'.
 --Muestra los nombres y los correos electrónicos de los empleados que pertenecen al departamento 'Engineering'.
---Cuenta cuántos productos hay en cada subcategoría y ordénalos por este conteo.
+--Cuenta cuántos productos hay en cada subcategoría y ordénalos por este conteo. //
 
 Select * from HumanResources.Employee
 
@@ -31,6 +31,14 @@ Select count(*)  ClientesTotales from Sales.Customer
 
 Select * from Production.ProductCategory Name 
 inner join Production.ProductSubcategory on Name.ProductCategoryID = ProductSubcategoryID
+
+Select count(*)  Totales from Production.ProductSubcategory
+
+--SELECT * FROM Sales.Customer
+--WHERE  IN ('LONDON')
+
+SELECT * FROM Production.ProductSubcategory
+ORDER BY ProductCategoryID
 
 
 
